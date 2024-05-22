720a8efd4129a0ef45ca3c16071dbb3d5c3d3d21
M4th12 <mattia.caldera04@gmail.com>
2024-05-22 12:47:27 +0200

    Added Privacy.php

--- Pages/infoSinistro.php
+++ Pages/infoSinistro.php
@@ -17,241 +17,10 @@
 </head>
 
 <body>
-  <div class="skiplink">
-    <a class="visually-hidden-focusable" href="#main-container">Vai ai contenuti</a>
-    <a class="visually-hidden-focusable" href="#footer">Vai al footer</a>
-  </div>
-  <!-- /skiplink -->
-  <header class="it-header-wrapper" data-bs-target="#header-nav-wrapper" style="">
-    <div class="it-header-slim-wrapper">
-      <div class="container">
-        <div class="row">
-          <div class="col-12">
-            <div class="it-header-slim-wrapper-content">
-              <a class="d-lg-block navbar-brand" target="_blank" href="#"
-                aria-label="Vai al portale {La tua Regione} - link esterno - apertura nuova scheda"
-                title="Vai al portale {La tua Regione}">La tua Regione</a>
-              <div class="it-header-slim-right-zone" role="navigation">
-                <div class="nav-item dropdown">
-                  <button type="button" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"
-                    aria-controls="languages" aria-haspopup="true">
-                    <span class="visually-hidden">Lingua attiva:</span>
-                    <span>ITA</span>
-                    <svg class="icon">
-                      <use href="../assets/bootstrap-italia/dist/svg/sprites.svg#it-expand"></use>
-                    </svg>
-                  </button>
-                  <div class="dropdown-menu">
-                    <div class="row">
-                      <div class="col-12">
-                        <div class="link-list-wrapper">
-                          <ul class="link-list">
-                            <li>
-                              <a class="dropdown-item list-item" href="#"><span>ITA <span
-                                    class="visually-hidden">selezionata</span></span></a>
-                            </li>
-                            <li>
-                              <a class="dropdown-item list-item" href="#"><span>ENG</span></a>
-                            </li>
-                          </ul>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <a class="btn btn-primary btn-icon btn-full" href="#" data-element="personal-area-login">
-                  <span class="rounded-icon" aria-hidden="true">
-                    <!-- <svg class="icon icon-primary">
-                        <use xlink:href="../assets/images/GB.png"></use>
-                      </svg> -->
-                    <img src="../assets/images/GB.png" alt="user pic" />
-                  </span>
-                  <span class="d-none d-lg-block">Giulia Bianchi</span>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="it-nav-wrapper">
-      <div class="it-header-center-wrapper">
-        <div class="container">
-          <div class="row">
-            <div class="col-12">
-              <div class="it-header-center-content-wrapper">
-                <div class="it-brand-wrapper">
-                  <a href="http://sb01.eurobeta.it:8080/" title="Vai alla homepage" aria-label="Home Il tuo Comune">
-                    <svg class="icon" aria-hidden="true">
-                      <image xlink:href="http://sb01.eurobeta.it:8080/wp-content/uploads/2022/07/logo_comune.svg" width="82" height="82"></image>
-                    </svg>
-                    <div class="it-brand-text">
-                      <div class="it-brand-title">Il tuo Comune</div>
-                      <div class="it-brand-tagline d-none d-md-block">Un comune da vivere</div>
-                    </div>
-                  </a>
-                </div>
-                <div class="it-right-zone">
-                  <div class="it-socials d-none d-lg-flex">
-                    <span>Seguici su</span>
-                    <ul>
-                      <li>
-                        <a href="#" target="_blank">
-                          <svg class="icon icon-sm icon-white align-top">
-                            <use xlink:href="../assets/bootstrap-italia/dist/svg/sprites.svg#it-twitter"></use>
-                          </svg>
-                          <span class="visually-hidden">Twitter</span></a>
-                      </li>
-                      <li>
-                        <a href="#" target="_blank">
-                          <svg class="icon icon-sm icon-white align-top">
-                            <use xlink:href="../assets/bootstrap-italia/dist/svg/sprites.svg#it-facebook"></use>
-                          </svg>
-                          <span class="visually-hidden">Facebook</span></a>
-                      </li>
-                      <li>
-                        <a href="#" target="_blank">
-                          <svg class="icon icon-sm icon-white align-top">
-                            <use xlink:href="../assets/bootstrap-italia/dist/svg/sprites.svg#it-youtube"></use>
-                          </svg>
-                          <span class="visually-hidden">YouTube</span></a>
-                      </li>
-                      <li>
-                        <a href="#" target="_blank">
-                          <svg class="icon icon-sm icon-white align-top">
-                            <use xlink:href="../assets/bootstrap-italia/dist/svg/sprites.svg#it-telegram"></use>
-                          </svg>
-                          <span class="visually-hidden">Telegram</span></a>
-                      </li>
-                    </ul>
-                  </div>
-                  <div class="it-search-wrapper">
-                    <span class="d-none d-md-block">Cerca</span>
-                    <button class="search-link rounded-icon" type="button" data-bs-toggle="modal"
-                      data-bs-target="#search-modal" aria-label="Cerca nel sito">
-                      <svg class="icon">
-                        <use href="../assets/bootstrap-italia/dist/svg/sprites.svg#it-search"></use>
-                      </svg>
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="it-header-navbar-wrapper" id="header-nav-wrapper">
-        <div class="container">
-          <div class="row">
-            <div class="col-12">
-              <!--start nav-->
-              <nav class="navbar navbar-expand-lg has-megamenu" aria-label="Navigazione principale">
-                <button class="custom-navbar-toggler" type="button" aria-controls="nav4" aria-expanded="false"
-                  aria-label="Mostra/Nascondi la navigazione" data-bs-target="#nav4" data-bs-toggle="navbarcollapsible"
-                  role="navigation">
-                  <svg class="icon">
-                    <use href="../assets/bootstrap-italia/dist/svg/sprites.svg#it-burger"></use>
-                  </svg>
-                </button>
-                <div class="navbar-collapsable" id="nav4">
-                  <div class="overlay" style="display: none"></div>
-                  <div class="close-div">
-                    <button class="btn close-menu" type="button">
-                      <span class="visually-hidden">Nascondi la navigazione</span>
-                      <svg class="icon">
-                        <use href="../assets/bootstrap-italia/dist/svg/sprites.svg#it-close-big"></use>
-                      </svg>
-                    </button>
-                  </div>
-                  <div class="menu-wrapper">
-                    <a href="#" aria-label="home Il tuo Comune" class="logo-hamburger">
-                      <svg class="icon" aria-hidden="true">
-                        <image xlink:href="http://sb01.eurobeta.it:8080/wp-content/uploads/2022/07/logo_comune.svg" width="82" height="82"></image>
-                      </svg>
-                      <div class="it-brand-text">
-                        <div class="it-brand-title">Il tuo Comune</div>
-                      </div>
-                    </a>
-                    <ul class="navbar-nav" data-element="main-navigation">
-                      <li class="nav-item">
-                        <a class="nav-link" href="http://sb01.eurobeta.it:8080/amministrazione/" data-element="management">
-                          <span>Amministrazione</span>
-                        </a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="http://sb01.eurobeta.it:8080/novita/" data-element="news">
-                          <span>Novità</span>
-                        </a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link active" href="http://sb01.eurobeta.it:8080/servizi/" data-element="all-services">
-                          <span>Servizi</span>
-                        </a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="http://sb01.eurobeta.it:8080/vivere-il-comune/" data-element="news">
-                          <span>Vivere il Comune</span>
-                        </a>
-                      </li>
-                    </ul>
-                    <ul class="navbar-nav navbar-secondary">
-                      <li class="nav-item">
-                        <a class="nav-link" href="#">Agricoltura</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="#">Tempo libero</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="#">Istruzione</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="#" data-element="all-topics">
-                          <span class="fw-bold">Tutti gli argomenti...</span>
-                        </a>
-                      </li>
-                    </ul>
-                    <div class="it-socials">
-                      <span>Seguici su</span>
-                      <ul>
-                        <li>
-                          <a href="#" target="_blank">
-                            <svg class="icon icon-sm icon-white align-top">
-                              <use xlink:href="../assets/bootstrap-italia/dist/svg/sprites.svg#it-twitter"></use>
-                            </svg>
-                            <span class="visually-hidden">Twitter</span></a>
-                        </li>
-                        <li>
-                          <a href="#" target="_blank">
-                            <svg class="icon icon-sm icon-white align-top">
-                              <use xlink:href="../assets/bootstrap-italia/dist/svg/sprites.svg#it-facebook"></use>
-                            </svg>
-                            <span class="visually-hidden">Facebook</span></a>
-                        </li>
-                        <li>
-                          <a href="#" target="_blank">
-                            <svg class="icon icon-sm icon-white align-top">
-                              <use xlink:href="../assets/bootstrap-italia/dist/svg/sprites.svg#it-youtube"></use>
-                            </svg>
-                            <span class="visually-hidden">YouTube</span></a>
-                        </li>
-                        <li>
-                          <a href="#" target="_blank">
-                            <svg class="icon icon-sm icon-white align-top">
-                              <use xlink:href="../assets/bootstrap-italia/dist/svg/sprites.svg#it-telegram"></use>
-                            </svg>
-                            <span class="visually-hidden">Telegram</span></a>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </nav>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+  <header>
+    <?PHP
+    include ("../Pages/navbar.php")
+        ?>
   </header>
   <main>
     <div class="container" id="main-container">
@@ -260,18 +29,10 @@
           <div class="cmp-breadcrumbs" role="navigation">
             <nav class="breadcrumb-container">
               <ol class="breadcrumb p-0" data-element="breadcrumb">
-                <li class="breadcrumb-item"><a href="http://sb01.eurobeta.it:8080/">Home</a></li>
+                <li class="breadcrumb-item"><a href="../index.php">Home</a></li>
                 <li class="breadcrumb-item">
                   <span class="separator">/</span>
-                  <a href="http://sb01.eurobeta.it:8080/servizi">Servizi</a>
-                </li>
-                <li class="breadcrumb-item">
-                  <span class="separator">/</span>
-                  <a href="http://sb01.eurobeta.it:8080/servizi-categoria/tributi-finanze-e-contravvenzioni/">Tributi, finanze e contravvenzioni</a>
-                </li>
-                <li class="breadcrumb-item">
-                  <span class="separator">/</span>
-                  <a href="http://sb01.eurobeta.it:8080/servizio/pagamento-multa/">Pagamento multa</a>
+                  <a href="privacy.php">Informativa Privacy</a>
                 </li>
                 <li class="breadcrumb-item active" aria-current="page">
                   <span class="separator">/</span>Dati Genearli
@@ -366,7 +127,7 @@
                               <span class="d-flex align-items-center">
                                 Mostra tutto
                                 <svg class="icon icon-primary icon-sm">
-                                  <use href="assets/bootstrap-italia/dist/svg/sprites.svg#it-expand"></use>
+                                  <use href="../assets/bootstrap-italia/dist/svg/sprites.svg#it-expand"></use>
                                 </svg>
                               </span>
                             </button>
---
+++ Pages/privacy.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta http-equiv="Content-Language" content="en" />
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Pagamenti - Il tuo Comune</title>
+
+  <meta name="viewport" content="width=device-width,initial-scale=1" />
+  <meta name="description" content="" />
+  <link rel="stylesheet" href="../assets/bootstrap-italia/dist/css/bootstrap-italia-comuni.min.css" />
+  <link href="../assets/css/style.css" rel="stylesheet" />
+</head>
+
+<body>
+<header>
+    <?PHP
+    include ("../Pages/navbar.php")
+        ?>
+</header>       
+
+  <main>
+    <div class="container" id="main-container">
+      <div class="row justify-content-center">
+        <div class="col-12 col-lg-10">
+        <div class="cmp-breadcrumbs" role="navigation">
+            <nav class="breadcrumb-container">
+              <ol class="breadcrumb p-0" data-element="breadcrumb">
+                <li class="breadcrumb-item"><a href="../index.php">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">
+                  <span class="separator">/</span>Informativa Privacy
+                </li>
+              </ol>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-12 col-lg-10">
+          <h1 class="title-xxxlarge mb-4">Pagamento multa</h1>
+        </div>
+        <div class="col-12">
+          <div class="cmp-info-progress d-flex" data-progress="">
+            <!-- Desktop -->
+            <div class="info-progress-wrapper d-none d-lg-flex w-100 px-3 flex-column justify-content-end step-active">
+              <div class="info-progress-body d-flex justify-content-between align-self-end align-items-end w-100 py-3">
+                <span class="d-block h-100 title-medium text-uppercase">Informativa sulla privacy</span>
+              </div>
+            </div>
+            <!-- Desktop -->
+            <div class="info-progress-wrapper d-none d-lg-flex w-100 px-3 flex-column justify-content-end">
+              <div class="info-progress-body d-flex justify-content-between align-self-end align-items-end w-100 py-3">
+                <span class="d-block h-100 title-medium text-uppercase">Dati generali</span>
+              </div>
+            </div>
+            <!-- Desktop -->
+            <div class="info-progress-wrapper d-none d-lg-flex w-100 px-3 flex-column justify-content-end">
+              <div class="info-progress-body d-flex justify-content-between align-self-end align-items-end w-100 py-3">
+                <span class="d-block h-100 title-medium text-uppercase">Dati specifici del servizio</span>
+              </div>
+            </div>
+            <!-- Desktop -->
+            <div class="info-progress-wrapper d-none d-lg-flex w-100 px-3 flex-column justify-content-end">
+              <div class="info-progress-body d-flex justify-content-between align-self-end align-items-end w-100 py-3">
+                <span class="d-block h-100 title-medium text-uppercase">Riepilogo</span>
+              </div>
+            </div>
+
+            <!-- Mobile -->
+            <div class="iscrizioni-header d-lg-none w-100">
+              <h4 class="step-title d-flex align-items-center justify-content-between drop-shadow">
+                <span class="d-block d-lg-inline"> Informativa sulla privacy </span>
+                <span class="step">1/4</span>
+              </h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-12 col-lg-8">
+          <p class="text-paragraph mt-2 mt-lg-3 mb-3 mb-md-4">
+            Il Comune di Firenze gestisce i dati personali forniti e liberamente comunicati sulla base dell’articolo
+            13 del Regolamento (UE) 2016/679 General data protection regulation (Gdpr) e degli articoli 13 e
+            successive modifiche e integrazione del decreto legislativo (di seguito d.lgs) 267/2000 (Testo unico enti
+            locali).
+          </p>
+          <span class="text-paragraph">Per i dettagli sul trattamento dei dati personali consulta l’<a href="#"
+              class="t-primary">informativa sulla privacy.</a>
+          </span>
+
+          <div class="form-check mt-md-40">
+            <div class="checkbox-body mt-4 mb-3 mb-lg-40 d-flex flex-column">
+              <input type="checkbox" id="privacy" name="privacy-field" value="privacy-field" />
+              <label class="title-small-semi-bold pt-1" for="privacy">Ho letto e compreso l’informativa sulla
+                privacy</label>
+              <div class="form-feedback just-validate-error-label d-none" id="privacy-error">
+                Questo campo è richiesto
+              </div>
+            </div>
+          </div>
+          <button type="button" class="btn btn-primary mobile-full mb-2 mb-md-5" id="forward-button">
+            <span class="">Avanti</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </main>
+  <script>
+    window.__PUBLIC_PATH__ = '../assets/bootstrap-italia/dist/fonts';
+  </script>
+  <script src="../assets/bootstrap-italia/dist/js/bootstrap-italia.bundle.min.js"></script>
+  <script src="../assets/script/scripts.js"></script>
+  <script src="../assets/script/store.js"></script>
+  <script src="../assets/script/pages/privacy.js"></script>
+</body>
+
+</html>
